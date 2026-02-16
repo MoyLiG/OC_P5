355f85d06263a200ba98c38a21562289c9dabb00
Morgan Le Gall <moy.morgan@gmail.com>
2026-02-16 20:35:22 +0100

    Utilisation de variables d'environnement - Suppression de scripts de test inutiles - Modification de .github/workflows/run_tests.yml - Ajout de fichiers de test

---
+++ scripts/original_transfert_csv_mongodb.py
@@ -0,0 +1,29 @@
+import pandas as pd
+from pymongo import MongoClient
+
+def transfert_csv_mongodb(csv_file_path="/data/healthcare_dataset.csv", mongo_uri="mongodb://root:example@mongodb:27017/", db_name="P5", collection_name="dataset_donnees_medicales"):
+    try:
+        df = pd.read_csv(csv_file_path)
+
+        client = MongoClient(mongo_uri)
+        db = client[db_name]
+        collection = db[collection_name]
+
+        if collection.count_documents({}) > 0:
+            print(f"La collection '{collection_name}' contient déjà des données. Annulation de l'insertion pour éviter les doublons.")
+            return
+
+        data = df.to_dict(orient='records')
+        collection.insert_many(data)
+        print(f"{len(data)} documents insérés dans la collection '{collection_name}' de la base '{db_name}'.")
+
+    except Exception as e:
+        print(f"Une erreur est survenue : {e}")
+    finally:
+        if 'client' in locals():
+            client.close()
+
+if __name__ == "__main__":
+    print("Début de l'exécution de transfert_csv_mongodb.py")
+    transfert_csv_mongodb()
+    print("Fin de l'exécution de transfert_csv_mongodb.py")
